639cc9ae0be89fa958afe5951115617514638048
e22301038 <e22301038@IUTA-WS-TP02-05>
2024-05-31 14:27:00 +0200

    Ajoutez des fichiers projet.

---
+++ EvaluationMachine/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+static double[] NHarm ()
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    double s = 0,n = 0;
+    double[] THarm = new double[a];
+
+    for (int i = 0; i < a; i++)
+    {
+        n++;
+        s = s + (1 / n);
+        THarm[i] = s;
+    }
+
+    return THarm;
+}
+
+double[] t = NHarm ();
+for (int i = 0; i < t.Length; i++)
+{
+    Console.WriteLine($"{t[i]:F2}");
+}
